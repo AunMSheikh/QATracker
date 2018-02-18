36e6e7f44fc3e59b9ee2ac2637cc16840647f99a
Aun Sheikh <aun.m.sheikh@gmail.com>
2018-02-17 19:59:22 -0500

    added router dependency and login skeleton

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Homepage from './Components/Homepage';
+import LoginPage from './Components/LoginPage';
 
-const App = () => <div> Testing </div>
+const App = () => <div> 
+    <Route path="/" exact component={Homepage} />
+    <Route path="/login" exact component={LoginPage} />
+    </div>
 
 export default App;
