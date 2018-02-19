fe0213236dd6e3c942d0965d192a1d4622910cc3
Aun Sheikh <aun.m.sheikh@gmail.com>
2018-02-19 14:30:01 -0500

    update home page with semantic ui test class

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Homepage = () => <div>
+const Homepage = () => <div className={["ui container"]}>
         <h1>This is my HOMEPAGE </h1>
         <Link to="/login"> Login </Link>
     </div>
