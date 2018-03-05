4dcb29b4b65531a36e06c0be367de064c29982d9
Soulfx Technologies <sfx@Soulfxs-MacBook-Air.local>
2018-03-05 09:46:00 -0500

    completed login page

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container, Button, Form , Message } from 'semantic-ui-react';
+import '../Styles/LoginPage.css';
+import '../Styles/Global.css';
 
-const LoginPage = () => <div>
-        <h1> Login page </h1>
-    </div>
+class LoginPage extends Component {
+    state = {  }
+    render() {
+        return (
+            <Container className={'center-container'}>
+                <h1 className={'center-content'}>Login To QT </h1>
+                <Message>
+                    <Form>
+                        <Form.Field> 
+                            <label htmlFor="email"> Email </label> 
+                            <input placeholder='example@example.com' type={'Email'} /> 
+                        </Form.Field>
+                        <Form.Field> 
+                             <label htmlFor="password"> Password </label>
+                             <input placeholder='Password' type={'Password'}/> 
+                        </Form.Field>
+        
+                        <Form.Checkbox label='Remember me' />
+                        <Button type='submit'>Submit</Button>
+                    </Form>
+                </Message>
+            </Container>
+        );
+    }
+}
 
 export default LoginPage;
