beaf6b674c8b8943a15b961c940aa0b26269c4c6
Soulfx Technologies <sfx@Soulfxs-MacBook-Air.local>
2018-03-05 09:51:13 -0500

    updated router to add support for the registration page

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import LoginPage from './Components/LoginPage';
+import RegisterPage from './Components/RegisterPage';
 
 const App = () => <div> 
     <Route path="/" exact component={Homepage} />
     <Route path="/login" exact component={LoginPage} />
+    <Route path="/register" exact component={RegisterPage} />
     </div>
 
 export default App;
